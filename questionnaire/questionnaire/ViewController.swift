0c82f3622c3ad479910004cc0ce6196c866edc08
pattyzamsal <pazs1991@gmail.com>
2020-10-25 23:39:18 -0500

    Create basic project in xcode

---
+++ questionnaire/questionnaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  questionnaire
+//
+//  Created by Patricia Zambrano on 25/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
